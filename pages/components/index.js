12677e069fbe9b255034154f8c6023310e3de5c4
dkzhen <dani314123@gmail.com>
2023-06-17 06:58:01 +0700

    landingpage

---
+++ pages/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./home/Navbar";
+import Billing from "./home/Billing";
+import CardDeal from "./home/CardDeal";
+import Business from "./home/Business";
+import Clients from "./home/Clients";
+import CTA from "./home/CTA";
+import Stats from "./home/Stats";
+import Footer from "./home/Footer";
+import Testimonials from "./home/Testimonials";
+import Hero from "./home/Hero";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,22 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import styles from "../../styles/style";
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "../components";
 
-const Home = ({ message }) => {
-  const [apiMessage, setApiMessage] = useState(message);
-
-  useEffect(() => {
-    fetch('/api/hello')
-      .then((response) => response.json())
-      .then((data) => setApiMessage(data.message))
-      .catch((error) => console.error(error));
-  }, []);
-
-  return (
-    <div>
-      <h1>Next.js Frontend and Backend Tutorial</h1>
-      <p>{apiMessage}</p>
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
     </div>
-  );
-};
 
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+    
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
+  </div>
+);
 
-export default Home;
+export default App;
