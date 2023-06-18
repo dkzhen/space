9b281ae8eba3db2953859d2d72f079f233a1ea99
dkzhen <dani314123@gmail.com>
2023-06-18 21:49:19 +0700

    V1:fixed navbar bug screen

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -36,7 +36,7 @@ const Hero = () => {
       </div>
 
       <div className={`flex ${styles.flexCenter} md:my-0 my-10`}>
-        <Image src={robot} alt="billing" className="w-[100%] h-[100%] " />
+        {/* <Image src={robot} alt="billing" className="w-[100%] h-[100%] " /> */}
 
         {/* gradient start */}
         {/* <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
