93f4cc8ea127e111a566114dff82b90c4caa7b45
dkzhen <dani314123@gmail.com>
2023-06-19 00:17:34 +0700

    V1:fixed navbar & getStarted

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -14,41 +14,37 @@ const hideOnMdStyles = css`
 
 const Hero = () => {
   return (
-    <section
-      id="home"
-      className={`flex md:flex-row flex-col ${styles.paddingY}`}
-    >
-     <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
-    <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
-      <Image src={discount} alt="discount" className="w-[32px] h-[32px]" />
-      <p className={`${styles.paragraph} ml-2`}>
-        <span className="text-white">20%</span> Discount For{" "}
-        <span className="text-white">1 Month</span> Account
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+    <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+      <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+        <Image src={discount} alt="discount" className="w-[32px] h-[32px]" />
+        <p className={`${styles.paragraph} ml-2`}>
+          <span className="text-white">20%</span> Discount For{' '}
+          <span className="text-white">1 Month</span> Account
+        </p>
+      </div>
+
+      <div className="flex flex-row justify-between items-center w-full">
+        <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+          The Next <br className="sm:block hidden" />{' '}
+          <span className="text-gradient">Generation</span>{' '}
+        </h1>
+        <div className="md:flex hidden md:mr-4 mr-0">
+          <GetStarted />
+        </div>
+      </div>
+
+      <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+        Payment Method.
+      </h1>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Our team of experts uses a methodology to identify the credit cards most likely to fit your needs.
+        We examine annual percentage rates, annual fees.
       </p>
     </div>
-
-    <div className="flex flex-row justify-between items-center w-full">
-      <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-        The Next{" "}
-        <br className="sm:block hidden" />
-        <span className="text-gradient">Generation</span>
-      </h1>
-      <div className="md:flex hidden md:mr-4 mr-0">
-        <GetStarted />
-      </div>
+    <div className="flex" style={{ height: '100%' }}>
+      <Image src={robot} alt="billing" className="w-full h-full" />
     </div>
-
-    <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
-      Payment Method.
-    </h1>
-    <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-      Our team of experts uses a methodology to identify the credit cards most likely to fit your needs.
-      We examine annual percentage rates, annual fees.
-    </p>
-  </div>
-  <div className="flex">
-    <Image src={robot} alt="billing" className="w-full" />
-  </div>
 
       <div className={`flex ${styles.flexCenter} md:my-0 my-10 relative`}>
         {/* <Image src={robot} alt="billing" className="w-[100%] object-cover h-[100%] relative z-[5] " /> */}
