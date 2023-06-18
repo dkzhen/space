b8e1e635871190bceda44d3a8aff3b2dc28412a2
dkzhen <dani314123@gmail.com>
2023-06-18 22:06:35 +0700

    V1:fixed navbar bug screen

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -20,9 +20,9 @@ const Hero = () => {
             The Next <br className="sm:block hidden" />{" "}
             <span className="text-gradient">Generation</span>{" "}
           </h1>
-          {/* <div className="ss:flex hidden md:mr-4 mr-0">
+          <div className="ss:flex hidden md:mr-4 mr-0">
             <GetStarted />
-          </div> */}
+          </div>
         </div>
 
         <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
