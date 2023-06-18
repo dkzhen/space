05c82c29b4c827fd9c2b84b977a9fc8e85fd43cf
dkzhen <dani314123@gmail.com>
2023-06-19 00:00:51 +0700

    V1:fixed navbar & getStarted

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,21 +2,25 @@ import styles from "@/styles/style";
 import { discount, robot } from "@/public/assets";
 import GetStarted from "./GetStarted";
 import Image from "next/image";
-import { css } from '@emotion/react';
+import { css } from "@emotion/react";
 
 const hideOnMdStyles = css`
-display: block;
+  display: block;
 
   @media (max-width: 768px) {
     display: none;
   }
 `;
 
-
 const Hero = () => {
   return (
-    <section id="home" className={`flex sm:flex-row flex-col ${styles.paddingY}`}>
-      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+    <section
+      id="home"
+      className={`flex sm:flex-row flex-col ${styles.paddingY}`}
+    >
+      <div
+        className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
+      >
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
           <Image src={discount} alt="discount" className="w-[32px] h-[32px]" />
           <p className={`${styles.paragraph} ml-2`}>
@@ -43,23 +47,28 @@ const Hero = () => {
           most likely to fit your needs. We examine annual percentage rates,
           annual fees.
         </p>
-       
       </div>
-
+      <div>
+        <Image
+          src={robot}
+          alt="billing"
+          className="w-full "
+        />
+      </div>
       <div className={`flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-        <Image src={robot} alt="billing" className="w-[1/3] object-cover  relative z-[5] " />
+        {/* <Image src={robot} alt="billing" className="w-[100%] object-cover h-[100%] relative z-[5] " /> */}
 
         {/* gradient start */}
-        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+        {/* <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
         <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
-        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" /> */}
         {/* gradient end */}
       </div>
       <div className={`md:hidden flex ${styles.flexCenter}`}>
-      <div className="sm:hidden">
-        <GetStarted />
+        <div className="sm:hidden">
+          <GetStarted />
+        </div>
       </div>
-    </div>
     </section>
   );
 };
